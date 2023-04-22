94b7c5036d5f260dc1e4f4688b57199b2d08c22a
BeamedPhoenix <131560283+BeamedPhoenix@users.noreply.github.com>
2023-04-23 00:02:32 +0100

    Add files via upload

---
+++ 𝕻𝖍𝖔𝖊𝖓𝖎𝖝 𝖎𝖒𝖆𝖌𝖊 𝖌𝖗𝖆𝖇𝖇𝖊𝖗/Build.bat
@@ -0,0 +1,73 @@
+hello https://github.com/BeamedPhoenix for more 
+
+hello https://github.com/BeamedPhoenix for more 
+hello https://github.com/BeamedPhoenix for more 
+hello https://github.com/BeamedPhoenix for more 
+hello https://github.com/BeamedPhoenix for more 
+
+hello https://github.com/BeamedPhoenix for more 
+hello https://github.com/BeamedPhoenix for more 
+hello https://github.com/BeamedPhoenix for more 
+
+hello https://github.com/BeamedPhoenix for more 
+hello https://github.com/BeamedPhoenix for more 
+
+hello https://github.com/BeamedPhoenix for more 
+hello https://github.com/BeamedPhoenix for more 
+hello https://github.com/BeamedPhoenix for more 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pip install -r requirements.txt
